8dc28a1d343e7d43b50f20ac0a2e6ed62634ceb9
l3iggs <l3iggs@users.noreply.github.com>
2015-07-31 08:29:42 +0200

    fix in case data dir does not yet exist

--- fixPerms.sh
+++ fixPerms.sh
@@ -1,15 +1,14 @@
 ocpath='/usr/share/webapps/owncloud'
 htuser='http'
 htgroup='http'
-mkdir ${ocpath}/data/
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
 find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
 chown -R root:${htuser} ${ocpath}/
 chown -R ${htuser}:${htgroup} ${ocpath}/apps/
 chown -R ${htuser}:${htgroup} ${ocpath}/config/
-chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/ || true
 chown -R ${htuser}:${htgroup} ${ocpath}/themes/
 chown root:${htuser} ${ocpath}/.htaccess
-chown root:${htuser} ${ocpath}/data/.htaccess
+chown root:${htuser} ${ocpath}/data/.htaccess || true
 chmod 0644 ${ocpath}/.htaccess
-chmod 0644 ${ocpath}/data/.htaccess
+chmod 0644 ${ocpath}/data/.htaccess || true
