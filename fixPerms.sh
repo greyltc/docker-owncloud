49e458eae3585e735758715d23b485f49af0e24d
l3iggs <l3iggs@users.noreply.github.com>
2015-11-27 11:45:42 -0500

    update to reflect OC 8.2 documentation

--- fixPerms.sh
+++ fixPerms.sh
@@ -1,14 +1,20 @@
-ocpath='/usr/share/webapps/owncloud'
-htuser='http'
-htgroup='http'
+#!/bin/bash
+ocpath='/var/www/owncloud'
+htuser='www-data'
+htgroup='www-data'
+rootuser='root' # On QNAP this is admin
+
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
 find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
-chown -R root:${htuser} ${ocpath}/
+
+chown -R ${rootuser}:${htgroup} ${ocpath}/
 chown -R ${htuser}:${htgroup} ${ocpath}/apps/
 chown -R ${htuser}:${htgroup} ${ocpath}/config/
-chown -R ${htuser}:${htgroup} ${ocpath}/data/ || true
+chown -R ${htuser}:${htgroup} ${ocpath}/data/
 chown -R ${htuser}:${htgroup} ${ocpath}/themes/
-chown root:${htuser} ${ocpath}/.htaccess
-chown root:${htuser} ${ocpath}/data/.htaccess || true
+
+chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+
 chmod 0644 ${ocpath}/.htaccess
-chmod 0644 ${ocpath}/data/.htaccess || true
+chmod 0644 ${ocpath}/data/.htaccess
