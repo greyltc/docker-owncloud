1c87b77935283bf0316afa9b1aa15dc40070eae2
l3iggs <l3iggs@users.noreply.github.com>
2015-11-27 11:50:38 -0500

    apply previous fixes

--- fixPerms.sh
+++ fixPerms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ocpath='/var/www/owncloud'
-htuser='www-data'
-htgroup='www-data'
+ocpath='/usr/share/webapps/owncloud'
+htuser='http'
+htgroup='http'
 rootuser='root' # On QNAP this is admin
 
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
@@ -10,11 +10,11 @@ find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
 chown -R ${rootuser}:${htgroup} ${ocpath}/
 chown -R ${htuser}:${htgroup} ${ocpath}/apps/
 chown -R ${htuser}:${htgroup} ${ocpath}/config/
-chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/ || true
 chown -R ${htuser}:${htgroup} ${ocpath}/themes/
 
 chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
-chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess || true
 
 chmod 0644 ${ocpath}/.htaccess
-chmod 0644 ${ocpath}/data/.htaccess
+chmod 0644 ${ocpath}/data/.htaccess || true
